70d0fce7489aee944f416510551d91029fa5a7de
Travis Roberts <travis.roberts2@gmail.com>
2017-12-08 22:28:32 -0600

    second commit

---
+++ assets/javascript/app.js
@@ -0,0 +1,23 @@
+$( window ).on('load', function() {
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCtS_aXBzOkMAnX94mlpNfhXwtZXGRdQf4",
+    authDomain: "train-scheduler-d0f6e.firebaseapp.com",
+    databaseURL: "https://train-scheduler-d0f6e.firebaseio.com",
+    projectId: "train-scheduler-d0f6e",
+    storageBucket: "train-scheduler-d0f6e.appspot.com",
+    messagingSenderId: "620291073146"
+  };
+  firebase.initializeApp(config);
+
+  var database = firebase.database();
+  
+
+
+
+
+
+
+
+// window load end
+});
